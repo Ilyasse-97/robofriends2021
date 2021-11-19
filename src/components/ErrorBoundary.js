89a98d02ceec02ce17fa13cdf79d6557ec7e2def
Hobocat <bouatou.ilyasse@gmail.com>
2021-11-19 18:52:52 +0100

    first time commiting robofriends 2021

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class ErrorBoundary extends Component {
+	constructor(){
+		super();
+		this.state= {
+			hasError: false
+		}
+	}
+}
+
+export default ErrorBoundary;
